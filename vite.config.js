3441784fd245a549b351028d686516d0f7a5d81d
Gmasek <masek.gergely98@gmail.com>
2025-05-19 11:51:46 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './', // important for static assets
+  base: '/prehab2rehab.github.io', // important for static assets
   build: {
     outDir: 'dist'
   }
