9d86d624a3ca6d98d90807ee3c5bb56e28992fe7
evastr <eva.strumegger@gmail.com>
2025-07-14 09:54:48 +0200

    move team to separate file

---
+++ src/locales/en/team.js
@@ -0,0 +1,74 @@
+import teamAllMD from './team-all.md?raw'
+
+const team = {
+  title: "Project Team",
+  LBI_DHP: {
+    name: "LBI for Digital Health and Prevention",
+    people: [
+      { role: "Project lead" },
+      { role: "Lead work package 2: Stakeholder involvement" },
+      { role: "Project administration" },
+      { role: "Research and development" },
+      { role: "Administration, communication" },
+      { role: "Research and development" },
+    ]
+  },
+  LBI_RR: {
+    people: [
+      { role: "" },
+      { role: "" },
+      { role: "" },
+      { role: "" }
+    ]
+  },
+  LBI_AR: {
+    name: "LBI for Arthritis and Rehabilitation",
+  },
+  PMU: {
+    name: "Paracelsus Medical University",
+    people: [
+      { role: "Lead work package 4: Feasibility studies" },
+      { role: "" }
+    ]
+  },
+  SALK: {
+    people: [
+      { role: "" },
+      { role: "" }
+    ]
+  },
+  ALP: {
+    people: [
+      { role: "Project management, software development" },
+      { role: "Project management, software development" },
+      { role: "Software development" },
+      { role: "Software development" },
+      { role: "Software development" },
+      { role: "Design, clickable prototypes" },
+      { role: "Software development" }
+    ]
+  },
+  BV: {
+    name: "Bad Vigaun medical health centre",
+    people: [
+      { role: "" }
+    ]
+  },
+  RL: {
+    people: [
+      { role: "" },
+      { role: "Software development" },
+      { role: "Software development" },
+      { role: "Software development" },
+      { role: "" }
+    ]
+  },
+  RDX: {
+    people: [
+      { role: "Project management, software development" }
+    ]
+  },
+  all: teamAllMD
+};
+
+export default team;
--- src/locales/en/translation.js
+++ src/locales/en/translation.js
@@ -6,7 +6,7 @@ import partnerMD from './partner.md?raw';
 import journeyMD from './journey.md?raw';
 import impressumMD from './impressum.md?raw';
 import privacyPolicyMD from './privacyPolicy.md?raw';
-import teamAllMD from './team-all.md?raw'
+import teamJS from "./team.js";
 
 const en = {
   menu: {
@@ -54,76 +54,7 @@ const en = {
     clinics: "Clinics",
     industry: "Industry"
   },
-  team: {
-    title: "Project Team",
-    LBI_DHP: {
-      name: "LBI for Digital Health and Prevention",
-      people: [
-        { role: "Project lead" },
-        { role: "Lead work package 2: Stakeholder involvement" },
-        { role: "Project administration" },
-        { role: "Research and development" },
-        { role: "Administration, communication" },
-        { role: "Research and development" },
-      ]
-    },
-    LBI_RR: {
-      people: [
-        { role: "" },
-        { role: "" },
-        { role: "" },
-        { role: "" }
-      ]
-    },
-    LBI_AR: {
-      name: "LBI for Arthritis and Rehabilitation",
-    },
-    PMU: {
-      name: "Paracelsus Medical University",
-      people: [
-        { role: "Lead work package 4: Feasibility studies" },
-        { role: "" }
-      ]
-    },
-    SALK: {
-      people: [
-        { role: "" },
-        { role: "" }
-      ]
-    },
-    ALP: {
-      people: [
-        { role: "Project management, software development" },
-        { role: "Project management, software development" },
-        { role: "Software development" },
-        { role: "Software development" },
-        { role: "Software development" },
-        { role: "Design, clickable prototypes" },
-        { role: "Software development" }
-      ]
-    },
-    BV: {
-      name: "Bad Vigaun medical health centre",
-      people: [
-        { role: "" }
-      ]
-    },
-    RL: {
-      people: [
-        { role: "" },
-        { role: "Software development" },
-        { role: "Software development" },
-        { role: "Software development" },
-        { role: "" }
-      ]
-    },
-    RDX: {
-      people: [
-        { role: "Project management, software development" }
-      ]
-    },
-    all: teamAllMD
-  },
+  team: teamJS,
   contact: {
     heading: "Contact",
     description: "If you have any questions, comments, or would like to participate in the project, feel free to contact us at any time.",
