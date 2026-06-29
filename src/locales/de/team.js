f1dce69f6a8b5c97ecf223bb781f8f03e19ff3b4
Daria <daria.kolosovskaia.lbidhp@gmail.com>
2026-06-29 23:29:26 +0200

    add team members RR

--- src/locales/de/team.js
+++ src/locales/de/team.js
@@ -24,6 +24,8 @@ const team = {
       { name: "Stefan Löfler", role: "", image: "Stefan-Loefler.jpg", rights: "© LBG / Nachtschatt" },
       { name: "Ferdinand Prüfer", role: "", image: "Ferdinand-Pruefer.jpg", rights: "© Julia Dragosits" },
       { name: "Chiara Vetrano", role: "", image: "Chiara-Vetrano.jpg", rights: "© Vetrano" }
+      { name: "Spela Matko", role: "", image: "Spela-Matko.jpg", rights: "© Julia Dragosits" }
+      { name: "Stefanie Hochenwarter", role: "", image: "Stefanie-Hochenwarter.jpeg", rights: "" }
     ]
   },
   LBI_AR: {
