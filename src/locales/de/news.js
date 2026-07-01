1b8f4334e5a622367481fc7b0f0696716e0db733
Daria <daria.kolosovskaia.lbidhp@gmail.com>
2026-07-01 10:31:45 +0200

    Update news text on BV study start

--- src/locales/de/news.js
+++ src/locales/de/news.js
@@ -4,7 +4,7 @@ const articles = [
    "headline": "📋 Studienstart in Bad Vigaun",
    "date": "30.06.2026",
    "image": "14-BV-study-started.jpg",
-   "text": "Ein wichtiger Meilenstein wurde erreicht: Die Machbarkeitsstudie am Medizinischen Zentrum Bad Vigaun hat offiziell begonnen! Die ersten Patient:innen erleben bereits die integrierte, digital unterstützte Prähabilitation vor ihrer Kniegelenksersatz-Operation. Wir sind gespannt, wie es weitergeht.  Mehr dazu im Post!",
+   "text": "Ein wichtiger Meilenstein wurde erreicht: Die Machbarkeitsstudie am Medizinischen Zentrum Bad Vigaun hat offiziell begonnen! Nach monatelanger intensiver Vorbereitung erleben die ersten Patient:innen bereits die integrierte, digital unterstützte Prähabilitation vor ihrer Kniegelenksersatz-Operation. 👇 Was alles dahintersteckt, erfahrt ihr im Post!",
    "linkedinUrl": "https://www.linkedin.com/feed/update/urn:li:activity:7477980967522086912",
    "videoUrl": ""
   },
